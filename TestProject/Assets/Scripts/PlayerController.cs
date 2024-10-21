0a6a78ec995ba215a175fce839fa6b70859c8395
TuanPham8Verse <tuanpham.8verse@gmail.com>
2024-10-21 10:52:59 +0700

    Update PlayerController

--- TestProject/Assets/Scripts/PlayerController.cs
+++ TestProject/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float jumpForge = 15f;
+    [SerializeField] private LayerMask layerMask;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position + new Vector3(0, -1), -Vector2.up);
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -1), -Vector2.up, 0.2f, layerMask);
+        if (hit)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForge);
+            }
+        }
     }
 }
